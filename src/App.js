99de6d7f26040d7af27681244cd68bf2a5ddb226
Kaito Sugimura <sugimurakaito@gmail.com>
2023-02-12 18:20:21 -0700

    Update file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Character from './Character/Character';
-import Gacha from './Gacha/Gacha';
-import Game from './Game/Game';
-import Home from './Home/Home';
-import Social from './Social/Social';
-import Nav from './_Components/Nav';
+import Character from './Pages/Character/Character';
+import Gacha from './Pages/Gacha/Gacha';
+import Game from './Pages/Game/Game';
+import Home from './Pages/Home/Home';
+import Social from './Pages/Social/Social';
+import Nav from './Components/Nav';
 
 function App() {
   return (

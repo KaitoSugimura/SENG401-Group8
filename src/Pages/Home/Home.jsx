99970e1c35e7fddc25beec04bb3f76c88086da84
Kaito Sugimura <sugimurakaito@gmail.com>
2023-02-17 16:38:52 -0700

    Create very basic home outline

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,23 @@
-import styles from './Home.module.css'
-import slimeImage from "../../../public/assets/GameArt/Slime.png"
+import styles from "./Home.module.css";
+import slimeImage from "../../../public/assets/GameArt/Slime.png";
 
 export default function Home() {
   return (
     <div className={styles.Home}>
-      <img src={slimeImage} alt="slime"/>
+      <div className={styles.MainBanner}>
+        <div className={styles.MBNameLevelContainer}>
+          <p className={styles.MBName}>Rimuru Tempest</p>
+          <p className={styles.MBLevel}>Lv.1</p>
+        </div>
+        <hr />
+        <p className={styles.MBRank}>Rank 200</p>
+      </div>
+      <img
+        src={slimeImage}
+        className={styles.characterIMG}
+        alt="slime"
+        draggable="false"
+      />
     </div>
-  )
+  );
 }

6c6f09847cf008a21f6bb8e2bd044e0429dd885e
Kaito Sugimura <sugimurakaito@gmail.com>
2023-02-17 19:12:56 -0700

    Create slime animation

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,27 +1,35 @@
 import styles from "./Home.module.css";
 import slimeImage from "../../../public/assets/GameArt/Slime.png";
+import chestOpen from "../../../public/assets/HomeIcons/chestOpen.png";
+import chestClosed from "../../../public/assets/HomeIcons/chestClosed.png";
+import { useState } from "react";
 
-const SnowflakeAmount = 60;
+const particleAmount = 60;
 const rows = [];
-for (var i = 0; i < SnowflakeAmount; i++) {
+for (var i = 0; i < particleAmount; i++) {
   const op = Math.random();
   const randSize = Math.random() * 20 + 5;
-  const snowStyle = {
+  const particlesStyle = {
     opacity: op,
     left: `${Math.random() * 100}vw`,
     width: `${randSize}px`,
     height: `${randSize}px`,
     animationTimingFunction: "ease-in-out",
     animationDuration: Math.random() * 8 + 2 + "s",
-    animationDelay:  Math.random() * 3 + "s",
+    animationDelay: Math.random() * 3 + "s",
     animationIterationCount: "infinite",
     animationDirection: "normal",
     animationFillMode: "forwards",
   };
-  rows.push(<div className={styles.Snow} key={i} style={snowStyle}></div>);
+  rows.push(
+    <div className={styles.Particles} key={i} style={particlesStyle}></div>
+  );
 }
 
 export default function Home() {
+  const [chestState, setChestState] = useState(false);
+  const [petted, setPetted] = useState(false);
+
   return (
     <div className={styles.Home}>
       <div className={styles.MainBanner}>
@@ -32,14 +40,31 @@ export default function Home() {
         <hr />
         <p className={styles.MBRank}>Rank 200</p>
       </div>
+      <button
+      onClick={(event) => {
+        setPetted(true);
+        setTimeout(()=>{setPetted(false)}, 1100);
+      }}
+      disabled={petted}>
       <img
         src={slimeImage}
-        className={styles.characterIMG}
+        className={`${styles.characterIMG} ${petted ? styles.applyShake : ""}`}
         alt="slime"
         draggable="false"
       />
+      </button>
 
-      <div className={styles.SnowWrap}>{rows}</div>
+      <img
+        src={chestState ? chestOpen : chestClosed}
+        className={styles.dailyChest}
+        alt="Daily chest click to open"
+        draggable="false"
+        onClick={() => {
+          setChestState(true);
+        }}
+      />
+
+      <div className={styles.ParticlesWrap}>{rows}</div>
     </div>
   );
 }

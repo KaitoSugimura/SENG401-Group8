aed91322a823f96404e4cace6543055326d4bfc4
Kaito Sugimura <sugimurakaito@gmail.com>
2023-02-18 14:49:18 -0700

    Fix character box alignments

--- src/Pages/Character/CharacterSelect.jsx
+++ src/Pages/Character/CharacterSelect.jsx
@@ -1,30 +1,38 @@
 import styles from "./Character.module.css";
 import { useState } from "react";
 
-const CharacterSelect = ({characters, switchCharacter}) => {
-    const [hover, setHover] = useState(false);
-    const imagePath = "";
+const CharacterSelect = ({ characters, switchCharacter }) => {
+  const [hover, setHover] = useState(false);
+  const imagePath = "";
 
-    const handleClick= (e)=>{
-        switchCharacter(e.currentTarget.getAttribute('number'));
-    }
+  const handleClick = (e) => {
+    switchCharacter(e.currentTarget.getAttribute("number"));
+  };
 
-    return ( 
-        <div className={styles.characterGrid}>
-            {characters.map(character =>(
-                <div  className = {styles.character} number = {character.id} key={character.id} onClick = {handleClick}>
-                    {character.unlocked ?(
-                        <img src={character.image} alt={character.type} key = {character.id}/>
-                    ):(
-                        <img src={"assets/GameArt/Locked.png"} alt={character.type} key = {character.id}/>
-                    )}
-                    <div></div>
-                    
-                </div>
-            ))}
-            
-        </div>
-     );
-}
- 
+  return (
+    <div className={styles.cgridContainer}>
+      <div className={styles.characterGrid}>
+        {characters.map((character) => (
+          <div
+            className={styles.character}
+            number={character.id}
+            key={character.id}
+            onClick={handleClick}
+          >
+            <img
+              src={
+                character.unlocked
+                  ? character.image
+                  : "assets/GameArt/Locked.png"
+              }
+              alt={character.type}
+              key={character.id}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default CharacterSelect;

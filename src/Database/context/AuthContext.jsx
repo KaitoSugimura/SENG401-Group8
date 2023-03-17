c1a0dd12e030f86aa57c20757db40d30e5ca9ae4
Danny Duong <dannyduong4.00@outlook.com>
2023-03-16 19:49:15 -0600

    Attach user data to data property of user

--- src/Database/context/AuthContext.jsx
+++ src/Database/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useReducer } from "react";
-import { projectAuth } from "../firebase/config";
+import { projectAuth, projectFirestore } from "../firebase/config";
+import firebase from "firebase";
 
 export const AuthContext = createContext();
 
@@ -23,10 +24,36 @@ export const AuthContextProvider = ({ children }) => {
   });
 
   useEffect(() => {
-    const unsub = projectAuth.onAuthStateChanged((user) => {
-      dispatch({ type: "AUTH_IS_READY", payload: user });
-      unsub();
+    const unsub = projectAuth.onAuthStateChanged(async (user) => {
+      if (user) {
+        let userData = await projectFirestore.collection("users").doc(user.uid).get();
+
+        if (!userData.data()) { // If user data doesn't exist
+          // Account is new: create user data
+          const userData = {
+            level: 31,
+            rank: 15,
+            musicVolume: 100,
+            gold: 1234,
+            gems: 2,
+            chestLastOpenedOn: firebase.firestore.Timestamp.fromMillis(Date.now() - 86400),
+            bannerFilepath: "/Account/Banners/Sky.jpg"
+          }
+
+          await projectFirestore.collection("users").doc(user.uid).set(userData);
+        }
+
+        // Attach user information (level, gold, etc.) to data property of user
+        user = { ...user, data: userData.data() };
+
+        console.log(user);
+        dispatch({ type: "AUTH_IS_READY", payload: user });
+      }
     });
+
+    return () => {
+      unsub();
+    }
   }, []);
 
   return (

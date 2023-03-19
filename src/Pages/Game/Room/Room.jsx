d95c6cb5abb4d250c3db561868bf9a08b00d3fa1
JoshuaW13 <josh.walters@shaw.ca>
2023-03-19 17:05:03 -0600

    updated map frame ratio

--- src/Pages/Game/Room/Room.jsx
+++ src/Pages/Game/Room/Room.jsx
@@ -5,12 +5,12 @@ import { AuthContext } from "../../../Database/context/AuthContext";
 
 export default function Room({setGameState}) {
   const { user } = useContext(AuthContext);
-  const battleFieldWidth = useRef(50); 
-  const battleFieldHeight = useRef(28.125);
+  const battleFieldWidth = useRef(16); 
+  const battleFieldHeight = useRef(9);
 
   useEffect(()=>{
-    battleFieldWidth.current = 50;
-    battleFieldHeight.current = 28.125;
+    battleFieldWidth.current = 16;
+    battleFieldHeight.current = 9;
     const ratio = (window.innerHeight - 65) / window.innerWidth;
     if (ratio < 0.5625) {
       battleFieldWidth.current = battleFieldWidth.current * (ratio / 0.5625);

a99659f6b26699878da58f96110ff5169f8ced14
JoshuaW13 <josh.walters@shaw.ca>
2023-03-19 15:46:30 -0600

    added some stuff to room

--- src/Pages/Game/Room/Room.jsx
+++ src/Pages/Game/Room/Room.jsx
@@ -1,10 +1,17 @@
+import Player from "./Player";
 import styles from "./Room.module.css";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../Database/context/AuthContext";
 
 export default function Room({setGameState}) {
+  const { user } = useContext(AuthContext);
   return (
-    <div>
-      This is a Room
-      <button onClick={() => setGameState("Battle")}>Start Battle</button>
+    <div className={styles.roomPage}>
+      <Player number={1} user={user}></Player>
+      <div className={styles.match}>
+        <button onClick={() => setGameState("Battle")}>Start Battle</button>
+      </div>
+      <Player number={2} user={user}></Player>
     </div>
   )
 }

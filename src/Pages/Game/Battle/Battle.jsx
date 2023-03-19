1aec773a6cf1cb7d7cb3262ac86bd28a652a32ba
Kaito Sugimura <sugimurakaito@gmail.com>
2023-03-19 00:49:55 -0600

    update game

--- src/Pages/Game/Battle/Battle.jsx
+++ src/Pages/Game/Battle/Battle.jsx
@@ -2,7 +2,7 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import { projectDatabase } from "../../../Database/firebase/config";
 import { useAuthContext } from "../../../Database/Hooks/useAuthContext";
 import styles from "./Battle.module.css";
-import slime from "/assets/GameArt/EarthSlime/EarthSlime1.gif";
+import slime from "/assets/GameArt/IceSlime/IceSlime1.gif";
 
 export default function Battle({ setGameState }) {
   const { user } = useAuthContext();
@@ -12,9 +12,12 @@ export default function Battle({ setGameState }) {
 
   const up = useRef(false);
   const left = useRef(false);
-  const down= useRef(false);
+  const down = useRef(false);
   const right = useRef(false);
   const intervalRef = useRef(null);
+
+  const battleFieldWidth = useRef(90); // If these numbers are to be changed, change in the useEffect below
+  const battleFieldHeight = useRef(50.625);
 
   let playerId;
   let playerRef;
@@ -27,39 +30,42 @@ export default function Battle({ setGameState }) {
   // Calculate battle field dimensions
   // (window.innerHeight - 65)/window.innerWidth
 
-  // if(window.innerWidth*0.5625)
+  useEffect(() => {
+    battleFieldWidth.current = 90;
+    battleFieldHeight.current = 50.625;
+    const ratio = (window.innerHeight - 65) / window.innerWidth;
+    if (ratio < 0.5625) {
+      battleFieldWidth.current = battleFieldWidth.current * (ratio / 0.5625);
+      battleFieldHeight.current = battleFieldWidth.current * 0.5625;
+    }
+  }, [window.innerWidth, window.innerHeight]);
 
-  const battleFieldWidth = 71;
-  const battleFieldHeight = 40;
-
-  const handleKeyPress = 
-    (xChange = 0, yChange = 0) => {
-      if (xChange != 0 || yChange != 0) {
-        const p = self.current;
-        if (xChange != 0 && yChange != 0) {
-          p.left = p.left + xChange * 1;
-          p.top = p.top + yChange * 1;
-        } else {
-          p.left = p.left + xChange * 1.25;
-          p.top = p.top + yChange * 1.25;
-        }
-
-        if (xChange > 0) {
-          p.direction = "right";
-        } else if (xChange < 0) {
-          p.direction = "left";
-        }
-
-        p.left = Math.min(Math.max(p.left, 0), battleFieldWidth);
-        p.top = Math.min(Math.max(p.top, 0), battleFieldHeight);
-
-        playerRef.set(p);
-        self.current = p;  
-        ss({...p});                            ` `
-        
-        console.log("FAAA:" , self.current);
+  const handleKeyPress = (xChange = 0, yChange = 0) => {
+    if (xChange != 0 || yChange != 0) {
+      const p = self.current;
+      if (xChange != 0 && yChange != 0) {
+        p.left = p.left + xChange * 1;
+        p.top = p.top + yChange * 1;
+      } else {
+        p.left = p.left + xChange * 1.25;
+        p.top = p.top + yChange * 1.25;
       }
-    };
+
+      if (xChange > 0) {
+        p.direction = "right";
+      } else if (xChange < 0) {
+        p.direction = "left";
+      }
+
+      p.left = Math.min(Math.max(p.left, 2.1), battleFieldWidth.current - 2.5);
+      p.top = Math.min(Math.max(p.top, 1.2), battleFieldHeight.current - 2);
+
+      playerRef.set(p);
+      self.current = p;
+      ss({ ...p });
+      ` `;
+    }
+  };
 
   useEffect(() => {
     // Initialize game
@@ -67,12 +73,17 @@ export default function Battle({ setGameState }) {
       id: playerId,
       name: user.displayName,
       direction: "right",
-      top: 0,
-      left: 0,
+      top: 1.2,
+      left: 2.1,
     });
     playerRef.onDisconnect().remove();
 
-    self.current = { top: 0, left: 0, direction: "right", name: user.displayName };
+    self.current = {
+      top: 1.2,
+      left: 2.1,
+      direction: "right",
+      name: user.displayName,
+    };
     ss(self.current);
   }, []);
 
@@ -173,34 +184,39 @@ export default function Battle({ setGameState }) {
         >
           End Battle
         </button>
-        <div className={styles.battleField}
-        style={{
-          width: battleFieldWidth + "vw",
-          height: battleFieldHeight + "vw",
-        }}
-        >
+        <div className={styles.battleFieldContainer}>
           <div
-            className={styles.character}
+            className={styles.battleField}
             style={{
-              top: s.top + "vw",
-              left: s.left + "vw",
+              width: battleFieldWidth.current + "vw",
+              height: battleFieldHeight.current + "vw",
             }}
-            data-direction={s.direction}
           >
-            <img src={slime} className={styles.slimeImage}></img>
-            <p className={styles.characterName}>{s.name}</p>
-          </div>
-          {enemy&& <div
+            <div
               className={styles.character}
               style={{
-                top: enemy.top + "vw",
-                left: enemy.left + "vw",
+                top: s.top + "vw",
+                left: s.left + "vw",
               }}
-              data-direction={enemy.direction}
+              data-direction={s.direction}
             >
               <img src={slime} className={styles.slimeImage}></img>
-              <p className={styles.characterName}>{enemy.name}</p>
-            </div>}
+              <p className={styles.characterName}>{s.name}</p>
+            </div>
+            {enemy && (
+              <div
+                className={styles.character}
+                style={{
+                  top: enemy.top + "vw",
+                  left: enemy.left + "vw",
+                }}
+                data-direction={enemy.direction}
+              >
+                <img src={slime} className={styles.slimeImage}></img>
+                <p className={styles.characterName}>{enemy.name}</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>

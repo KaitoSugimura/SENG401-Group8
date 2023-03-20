38c8461a587e1d41830472c30da8609a5cc2c964
JoshuaW13 <josh.walters@shaw.ca>
2023-03-19 20:42:59 -0600

    added game map

--- src/Pages/Game/Room/Room.jsx
+++ src/Pages/Game/Room/Room.jsx
@@ -24,8 +24,7 @@ export default function Room({setGameState}) {
       <div className={styles.match}>
         <div className={styles.map}
         style={{width:battleFieldWidth.current + "vw",height: battleFieldHeight.current + "vw",}}>
-          <img src="" alt="" /> 
-          {/* ADD MAP IMAGE HERE! */}
+          <img src="assets/GameMap/SlimeMeadows.svg" alt="" /> 
         </div>
         <div className={styles.goldContainer}>
           <p className={styles.mapName}>Slime Meadows</p>

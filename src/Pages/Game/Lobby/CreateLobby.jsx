2d70cb0689c1df153204cae6bf957c3425bb41cf
JoshuaW13 <josh.walters@shaw.ca>
2023-03-21 10:25:48 -0600

    fixed lobby creation

--- src/Pages/Game/Lobby/CreateLobby.jsx
+++ src/Pages/Game/Lobby/CreateLobby.jsx
@@ -1,21 +1,88 @@
 import { useContext, useEffect, useState } from "react";
 import styles from "./Lobby.module.css";
-const CreateLobby = () => {
-    const[type, setType]=useState("");
+
+
+const CreateLobby = ({setPopUp, createPublicRoom, createPrivateRoom}) => {
+    const arenaTypes = ['Public', 'Private'];
+    const[type, setType]=useState("public");
+    const[password, setPassword]= useState("");
+    const[gold,setGold]=useState(100);
 
     const handleSubmit = (event)=>{
         event.preventDefault();
-        alert(`name entered was: ${type}`);
+        if(type==='public'){
+            createPublicRoom(gold);
+        }
+        else if(type==='private'){
+            createPrivateRoom(password, gold);
+        } 
     }
+
+    const typeHandler= (event)=>{
+        const type = event.target.value;
+        if(type==="Public"){
+            setType("public");
+            setPassword("");
+        }
+        else if(type==="Private"){
+            setType('private');
+        }
+    }
+
+    const passwordHandler=(event)=>{
+        const password = event.target.value;
+        setPassword(password);
+    }
+
+    const goldHandler=(event)=>{
+        const gold = event.target.value;
+        setGold(gold);
+    }
+    
     return (
         <div className={styles.createLobby}>
-            <form onSubmit={handleSubmit}>
-                <label>Lobby Type
-                    <input type="text"
-                    value ={type} 
-                    onChange={(e)=> setType(e.target.value)}/>
-                </label>
-                <input type="submit" />
+            <p>Lobby Settings</p>
+            <form onSubmit={(e)=>{
+                handleSubmit(e);
+                setPopUp(false);                
+            }}>
+                <div className={styles.formFields}>
+                    <label>Lobby Type</label>
+                        <select onChange={typeHandler}>
+                            {arenaTypes.map((type, index) => {
+                                return <option key={index} >
+                                    {type}
+                                </option>
+                            })}
+                        </select>
+                        <div>
+                            <label>Gold Amount</label>
+                            <input
+                            type="number"
+                            min={100}
+                            value={gold}
+                            onChange={goldHandler}
+                            />
+                        </div>
+                        {type==='private'&&
+                            <div>
+                                <label>Password</label>
+                                <input
+                                type="text"
+                                maxLength={"10"}
+                                value={password}
+                                onChange={passwordHandler}
+                                />
+                            </div>
+                        }
+                    
+                    <div>
+                    <input type="submit"/>
+                    </div>
+                    
+
+                </div>
+                
             </form>
         </div>
         

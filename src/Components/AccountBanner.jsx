3d1d4f8e6cdd4124758d85ce49baddb69c4179ed
Kaito Sugimura <sugimurakaito@gmail.com>
2023-03-21 22:39:31 -0600

    Finish styling banners

--- src/Components/AccountBanner.jsx
+++ src/Components/AccountBanner.jsx
@@ -1,20 +1,23 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { AuthContext } from "../Database/context/AuthContext";
 import { useLogout } from "../Database/Hooks/useLogout";
-import styles from "./AccountBanner.module.css"
+import styles from "./AccountBanner.module.css";
 import Banner from "./Banner";
 import { projectAuth, projectFirestore } from "../Database/firebase/config";
 import firebase from "firebase";
 
-
 export default function AccountBanner({ setShowBanner, isNavBanner }) {
   const { logout } = useLogout();
-  const [userRef, setUserRef]=useState(null);
+  const [userRef, setUserRef] = useState(null);
   const { user } = useContext(AuthContext);
   const [bSelectionOn, setBSelectionOn] = useState(false);
-  const [banner, setBanner]=useState(user.data.bannerFilepath);
+  const [banner, setBanner] = useState(user.data.bannerFilepath);
 
-  const[banners,setBanners]=useState([
+  const [refresh, setRefresh] = useState(Date.now());
+
+  const scrollPane = useRef(null);
+
+  const [banners, setBanners] = useState([
     "/Account/Banners/Sky.jpg",
     "/Account/Banners/cloudyMountains.jpg",
     "/Account/Banners/Sky.jpg",
@@ -26,42 +29,100 @@ export default function AccountBanner({ setShowBanner, isNavBanner }) {
     "/Account/Banners/Sky.jpg",
   ]);
 
-  useEffect(()=>{
-    const userRef= projectFirestore.collection("users").doc(user.uid);
+  useEffect(() => {
+    const userRef = projectFirestore.collection("users").doc(user.uid);
     setUserRef(userRef);
-  },[]);
+  }, []);
 
-  const changeBanner=(bannerPath)=>{
-    userRef.update({bannerFilepath: bannerPath});
+  const changeBanner = (bannerPath) => {
+    userRef.update({ bannerFilepath: bannerPath });
     setBanner(bannerPath);
+  };
+
+  useEffect(() => {
+    setInterval(() => {
+      setRefresh(Date.now());
+    }, 300);
+  }, []);
+
+  let centerIndex;
+  if (scrollPane.current) {
+    const maxScrollTop =
+      scrollPane.current.scrollHeight - scrollPane.current.clientHeight;
+    const scrollRatio = scrollPane.current.scrollTop / maxScrollTop;
+    centerIndex = Math.floor(banners.length * scrollRatio);
   }
 
   return (
-    <div className={styles.AccountBanner} >
+    <div className={styles.AccountBanner}>
       <div className={styles.bannerInfo}>
         <h1 className={styles.Handle}>{user.displayName}</h1>
-        {isNavBanner && <div className={styles.editBannerButton} onClick={()=>{setBSelectionOn(true)}}>
-          <img src="/Account/editBanner.png" alt="" />
-        </div> }     
-
+        {isNavBanner && (
+          <div
+            className={styles.editBannerButton}
+            onClick={() => {
+              setBSelectionOn(true);
+            }}
+          >
+            <img src="/Account/editBanner.png" alt="" />
+          </div>
+        )}
       </div>
-            
-      <img src={user.data.slimePath+".svg"} className={styles.character}></img>
-      <p className={styles.Rank}>Rank: {user.data.rank}</p>
-      <p className={styles.Status}>Hello my name is {user.displayName}. I'm not a bad Slime!</p>
-      {isNavBanner && <button className={styles.SignOut} onClick={() => { if (setShowBanner) setShowBanner(false); logout() }}>Sign out</button>}
 
-      <img src={banner} className={styles.banner} ></img>
-      
-      {isNavBanner && <div className={`${styles.bannerSelectionContainer} ${bSelectionOn ? styles.ShowBannerSelection : ""}`}>
-        <div className={styles.bannerOptions} style={{backgroundImage: `url(${banner})`}}>
-          
-          {banners.map((bannerx, index)=>(
-            <Banner index = {index} banner={bannerx} setBanner={changeBanner} banners={banners}></Banner>
-          ))}
+      <img
+        src={user.data.slimePath + ".svg"}
+        className={styles.character}
+      ></img>
+      <p className={styles.Rank}>Rank: {user.data.rank}</p>
+      <p className={styles.Status}>
+        Hello my name is {user.displayName}. I'm not a bad Slime!
+      </p>
+      {isNavBanner && (
+        <button
+          className={styles.SignOut}
+          onClick={() => {
+            if (setShowBanner) setShowBanner(false);
+            logout();
+          }}
+        >
+          Sign out
+        </button>
+      )}
+
+      <img src={banner} className={styles.banner}></img>
+
+      {isNavBanner && (
+        <div
+          className={`${styles.bannerSelectionContainer} ${
+            bSelectionOn ? styles.ShowBannerSelection : ""
+          }`}
+        >
+          <div
+            className={styles.bannerOptions}
+            style={{ backgroundImage: `url(${banner})` }}
+            ref={scrollPane}
+          >
+            {banners.map((bannerx, index) => (
+              <Banner
+                index={index}
+                banner={bannerx}
+                setBanner={changeBanner}
+                banners={banners}
+                centerIndex={centerIndex}
+                key={index}
+              ></Banner>
+            ))}
+          </div>
+          <button
+            className={styles.bannerSelectionExit}
+            onClick={() => {
+              setBSelectionOn(false);
+            }}
+          >
+            X
+          </button>
         </div>
-        <button className={styles.bannerSelectionExit} onClick={() => { setBSelectionOn(false); }}>X</button>
-      </div>}
+      )}
     </div>
-  )
+  );
 }
--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,11 +1,40 @@
+
+import { useRef } from "react";
 import styles from "./AccountBanner.module.css"
-const Banner = ({index, banner, setBanner, banners}) => {
+const Banner = ({index, banner, setBanner, banners, centerIndex}) => {
+
+    const lastSlashIndex = banner.lastIndexOf('/');
+    const name = banner.substring(lastSlashIndex + 1);
+    const lastPeriodIndex = banner.substring(lastSlashIndex + 1).lastIndexOf('.');
+    const ImageName = name.slice(0, lastPeriodIndex);
+
+    const width = window.innerWidth * 0.4;
+    const widthStep = window.innerWidth * 0.02;
+
+    const distanceFromCenter = Math.abs(index - centerIndex);
+    const thisBannerWidth = width - widthStep * distanceFromCenter;
+
+    const scrollRef = useRef(null);
+
     return ( 
-        <div className={styles.bannerRow}>
+        <div className={styles.bannerRow} style={{
+            width: thisBannerWidth?thisBannerWidth:0,
+            marginTop: index===0?"15vh":"",
+            marginBottom: index===banners.length-1?"25vh":""
+        }}
+        ref={scrollRef}
+        onClick={() => {
+            console.log("AUGHHH");
+            if (scrollRef.current) {
+                scrollRef.current.scrollIntoView({ behavior: 'smooth', block: 'center'  });
+              }
+          }}
+        >
             <div className={styles.bannerContainer} onClick={()=>{
                 setBanner(banners[index]);
             }}>
                 <img src={banner} alt="" />
+                <p className={styles.bannerName}>{ImageName}</p>
             </div>            
         </div>
         

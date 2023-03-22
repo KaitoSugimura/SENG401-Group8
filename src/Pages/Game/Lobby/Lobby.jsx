1b57d1698e669c7bce57c47e4a8504c4822edd4c
Kaito Sugimura <sugimurakaito@gmail.com>
2023-03-22 14:18:41 -0600

    Style lobby buttons

--- src/Pages/Game/Lobby/Lobby.jsx
+++ src/Pages/Game/Lobby/Lobby.jsx
@@ -124,7 +124,7 @@ export default function Lobby({ setGameState }) {
               </div>
             </div>
           ))}
-          {!roomList && <p style={{marginTop: "3vh"}}>No rooms to display</p>}
+          {!roomList && <p className={styles.noRoomsText}>No rooms to display</p>}
 
 
 
@@ -141,13 +141,16 @@ export default function Lobby({ setGameState }) {
         Ranked
         </button>}
         {!mode&&lobbyList&&!popup && <div className={styles.lobbySelection}>
-            <button className={styles.createButton} onClick={() => { showRoomOptions() }}>
+            <button className={`${styles.createButton} ${styles.createLobbyButton}`} onClick={() => { showRoomOptions() }}>
             
             Create Lobby
            </button>
            <div className={styles.returnButton} onClick={()=>{setMode(true);
             setLobbyList(false);}}>
-            <img src="assets/GameArt/RestartButton.png" alt="" /></div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+</svg>
+</div>
           </div>}
         
 

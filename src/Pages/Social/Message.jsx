0f8c700a16ad344d3d1bf2bf4c251d54d7f06cc5
Kaito Sugimura <sugimurakaito@gmail.com>
2023-03-23 22:34:45 -0600

    Adjust banner pos in social page

--- src/Pages/Social/Message.jsx
+++ src/Pages/Social/Message.jsx
@@ -10,6 +10,9 @@ const Message = ({ message }) => {
   const [senderData, setSenderData] = useState(null);
   const [showBanner, setShowBanner] = useState(false);
   const bannerRef = useRef(null);
+  const messageRef = useRef(null);
+
+  const [topAdjustment, setTopAdjustment] = useState(1);
 
   // Close banner upon clicking outside
   useEffect(() => {
@@ -17,13 +20,13 @@ const Message = ({ message }) => {
       if (bannerRef.current && !bannerRef.current.contains(e.target)) {
         setShowBanner(false);
       }
-    }
+    };
 
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    }
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
   }, [bannerRef]);
 
   // After senderData is fetched (from clicking the sender's slime), show banner
@@ -31,39 +34,67 @@ const Message = ({ message }) => {
     if (senderData) {
       setShowBanner(true);
     }
-  }, [senderData])
+  }, [senderData]);
 
   const fetchBanner = async () => {
-    const data = await projectFirestore.collection("users").doc(id).get().then(res => res.data());
+    const data = await projectFirestore
+      .collection("users")
+      .doc(id)
+      .get()
+      .then((res) => res.data());
     const { slimeType, slimeSkin } = data;
     setSenderData({
       ...data,
       slimePath: `assets/GameArt/${slimeType}Slime/${slimeType}Slime${slimeSkin}`,
     });
-  }
+
+    if (messageRef.current) {
+      const messageRect = messageRef.current.getBoundingClientRect();
+      const possibleTopAdjustment =
+        window.innerHeight - messageRect.top - window.innerWidth / 3.6;
+      if (possibleTopAdjustment < 0) {
+        setTopAdjustment(possibleTopAdjustment);
+      }
+    }
+  };
 
   return (
-    <div className={`${styles.messageContainer} ${username === user.data.username ? styles.mine : ""}`}>
+    <div
+      ref={messageRef}
+      className={`${styles.messageContainer} ${
+        username === user.data.username ? styles.mine : ""
+      }`}
+    >
       <button onClick={fetchBanner}>
         <img src={`${slimePath}.svg`} alt="" />
       </button>
-      <div className={`${username === user.data.username ? styles.mine : ""}`} >
+      <div
+        className={`${username === user.data.username ? styles.mine : ""} ${
+          styles.messageFlex
+        }`}
+      >
         <p className={styles.name}>{username}</p>
         <p className={styles.message}>{content}</p>
       </div>
-      {showBanner &&
-        <div className={styles.AccountBannerContainer} ref={bannerRef}>
+      {showBanner && (
+        <div
+          className={styles.AccountBannerContainer}
+          ref={bannerRef}
+          style={{
+            top: topAdjustment + "px",
+          }}
+        >
           <AccountBanner
             setShowBanner={null}
             isNavBanner={false}
             data={senderData}
-            bannerWidth={"19"}
+            bannerWidth={"17"}
             widthUnits={"vw"}
           ></AccountBanner>
         </div>
-      }
-    </div >
+      )}
+    </div>
   );
-}
+};
 
 export default Message;

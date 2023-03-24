57b4755282d5eb85b679cb36ea94a085002b36a4
JoshuaW13 <josh.walters@shaw.ca>
2023-03-23 21:59:22 -0600

    updated victory screen

--- src/Pages/Game/EndScreen/EndScreen.jsx
+++ src/Pages/Game/EndScreen/EndScreen.jsx
@@ -1,21 +1,39 @@
 import styles from "./EndScreen.module.css";
 import slime from "/assets/GameArt/EarthSlime/EarthSlime1.gif";
 import PlayerBanner from "./PlayerBanner";
+import gold from "../../.././../public/assets/GameArt/Gold.png"
 import { useContext, useEffect, useRef, useState } from "react";
 import AccountBanner from "../../../Components/AccountBanner";
 
 export default function EndScreen({ setGameState }) {
+  // placeholder for if the win screen is ranked version or not
+  const[ranked,setRanked]=useState(true);
   return (
     <div className={styles.EndScreen}>
-      <p>+ 20 Rank Points</p>
+      <div className={styles.rewards}>
+        <p>+ 20 Rank Points</p>
+        <img src={gold} alt=""/>
+        <p>+ 100 Gold</p>
+      </div>
+      
       <PlayerBanner left={true} winner={true}></PlayerBanner>
       <PlayerBanner left={false} winner={false}></PlayerBanner>
-      <button
-        onClick={() => setGameState("Lobby")}
-        className={styles.returnToLobbyBtn}
-      >
-        Go back to Lobby in shame
-      </button>
+      <div className={styles.buttonContainer}>
+        {/* BOTH BUTTONS GO BACK TO LOBBY RN */}
+        <button
+          onClick={() => setGameState("Lobby")}
+          className={styles.returnToLobbyBtn}
+        >
+          Lobby
+        </button>
+        {ranked&&<button
+          onClick={() => setGameState("Lobby")}
+          className={styles.returnToLobbyBtn}
+        >
+          Play Again!
+        </button>}
+      </div>
+      
     </div>
   );
 }

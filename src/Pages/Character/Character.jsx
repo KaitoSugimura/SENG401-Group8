f254270567d0413218ecc7b1bec96ebf2682aa69
JoshuaW13 <josh.walters@shaw.ca>
2023-03-25 15:07:31 -0600

    added characters.jsx data

--- src/Pages/Character/Character.jsx
+++ src/Pages/Character/Character.jsx
@@ -3,28 +3,14 @@ import { AuthContext } from "../../Database/context/AuthContext";
 import CharacterSelect from './CharacterSelect';
 import styles from "./Character.module.css";
 import CharacterProfile from "./CharacterProfile";
-import { projectAuth, projectFirestore } from "../../Database/firebase/config";
+import characterData from "../../Database/JsxData/characters"
 
 export default function Character() {
 
   const { user, userRef } = useContext(AuthContext);
   const [userCharacters, updateUserCharacters] = useState([]);
-  const [characters, updateCharacters] = useState([
-    { type: "Normal", skin: 1, unlocked: true, power: 3, speed: 3, health: 3, two: false, three: false, price: 450,id: 1 },
-    { type: "Fire", skin: 1, unlocked: false, power: 5, speed: 3, health: 1, two: false, three: false, price: 450,id: 2 },
-    { type: "Ice", skin: 1, unlocked: false, power: 4, speed: 2, health: 3, two: false, three: false, price: 450,id: 3 },
-    { type: "Earth", skin: 1, unlocked: false, power: 4, speed: 1, health: 4, two: false, three: false, price: 450,id: 4 },
-    { type: "Air", skin: 1, unlocked: false, power: 3, speed: 5, health: 1, two: false, three: false, price: 450,id: 5 },
-    { type: "Electric", skin: 1, unlocked: false, power: 3, speed: 4, health: 2, two: false, three: false, price: 450,id: 6 },
-    { type: "Honey", skin: 1, unlocked: false, power: 3, speed: 2, health: 4, two: false, three: false, price: 900,id: 7 },
-    { type: "Poison", skin: 1, unlocked: false, power: 5, speed: 2, health: 2, two: false, three: false, price: 900,id: 8 },
-    { type: "Wild", skin: 1, unlocked: false, power: 4, speed: 3, health: 2, two: false, three: false, price: 900,id: 9 },
-    { type: "Shadow", skin: 1, unlocked: false, power: 3, speed: 4, health: 2, two: false, three: false, price: 900,id: 10 },
-    { type: "BubbleGum", skin: 1, unlocked: false, power: 2, speed: 2, health: 5, two: false, three: false, price: 900,id: 11 },
-    { type: "Armored", skin: 1, unlocked: false, power: 3, speed: 1, health: 5, two: false, three: false, price: 900,id: 12 },
-    { type: "Celestial", skin: 1, unlocked: false, power: 4, speed: 4, health: 1, two: false, three: false, price: 1800,id: 13 },
-    { type: "Metal", skin: 1, unlocked: false, power: 3, speed: 3, health: 3, two: false, three: false, price: 1800,id: 14 },
-  ])
+  let {charactersData} = characterData
+  const [characters, updateCharacters] = useState(charactersData);
 
   const [character, updateCharacter] = useState(
     characters[0]
@@ -32,7 +18,6 @@ export default function Character() {
 
   //fetch user slimes
   useEffect(()=>{
-    //console.log("Getting the slimes!")
     const slimes = user.data.slimes;
     updateUserCharacters(slimes);
     //console.log(userCharacters);

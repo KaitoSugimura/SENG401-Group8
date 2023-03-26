53cd673a5935d43ca550089cbbdd04a39cd09735
JoshuaW13 <josh.walters@shaw.ca>
2023-03-26 12:27:11 -0600

    added ranked queue screen

--- src/Pages/Game/Queue/Queue.jsx
+++ src/Pages/Game/Queue/Queue.jsx
@@ -14,6 +14,17 @@ export default function Queue({ setGameState }) {
   const { user } = useContext(AuthContext);
   const { setServerPlayerID, setClientPlayerID } = useContext(gameStateContext);
 
+  const tips = [
+    "Use WASD to move",
+    "Aim with mouse and shoot slimeballs with space",
+    "Use E to toggle buffed slimeball type",
+    "Slimeballs will become buffed after rebounding 3 times",
+    "green slimeballs heal and blue buff your attack stat",
+    "Characters have different strengths and weaknesses. Adapt your playstyle to fit."
+  ]
+  const[tip, setTip]=useState(tips[Math.floor(Math.random()*tips.length)])
+  const[tipIteration, setIteration]=useState(0);
+
   useEffect(() => {
     const queueRanked = async () => {
       const queueRef = projectDatabase.ref(`lobby/queue`);
@@ -58,28 +69,66 @@ export default function Queue({ setGameState }) {
 
     queueRanked();
   }, []);
+ 
 
   return (
     <div className={styles.roomPage}>
       <div className={styles.container}>
-        <div className={styles.loadingText}>
-          <span>Q</span>
-          <span style={{ animationDelay: `${0.1}s` }}>U</span>
-          <span style={{ animationDelay: `${0.2}s` }}>E</span>
-          <span style={{ animationDelay: `${0.3}s` }}>U</span>
-          <span style={{ animationDelay: `${0.4}s` }}>E</span>
-          <span style={{ animationDelay: `${0.5}s` }}>I</span>
-          <span style={{ animationDelay: `${0.6}s` }}>N</span>
-          <span style={{ animationDelay: `${0.7}s` }}>G</span>
-          <span style={{ animationDelay: `${0.8}s` }}>.</span>
-          <span style={{ animationDelay: `${0.9}s` }}>.</span>
-          <span style={{ animationDelay: `${1}s` }}>.</span>
+        <div className={styles.playerInfo}>
+          <h1>{user.displayName}</h1>
+          <img src={user.data.slimePath+".gif"} alt="slime" />
+          <div className={styles.rank}>
+            <h2>Rank: {user.data.rankPoints}</h2>
+          </div>
         </div>
-        <img
-          src={loadingSlime}
-          className={styles.loadingSlime}
-          style={{ animationDuration: `14s`, animationDelay: `${0.7}s` }}
-        ></img>
+        
+        <div className={styles.rightSide}>
+          <div className={styles.loadingText}>
+            <span>Q</span>
+            <span style={{ animationDelay: `${0.1}s` }}>U</span>
+            <span style={{ animationDelay: `${0.2}s` }}>E</span>
+            <span style={{ animationDelay: `${0.3}s` }}>U</span>
+            <span style={{ animationDelay: `${0.4}s` }}>E</span>
+            <span style={{ animationDelay: `${0.5}s` }}>I</span>
+            <span style={{ animationDelay: `${0.6}s` }}>N</span>
+            <span style={{ animationDelay: `${0.7}s` }}>G</span>
+            <span style={{ animationDelay: `${0.8}s` }}>.</span>
+            <span style={{ animationDelay: `${0.9}s` }}>.</span>
+            <span style={{ animationDelay: `${1}s` }}>.</span>
+          </div>
+          <div className={styles.loadingTips}>
+            {<span onAnimationIteration={()=>{
+              setIteration(tipIteration+1);
+              if(tipIteration%2==0){
+                setTip(tips[Math.floor(Math.random()*tips.length)]);
+              }
+              
+            }}>{tip}</span>}
+          </div>
+          <div
+                className={styles.returnButton}
+                onClick={() => {
+                  projectDatabase.ref(`lobby/queue/${user.uid}`).remove();
+                  setGameState("Lobby");
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+                  />
+                </svg>
+              </div>
+          
+        </div>
       </div>
     </div>
   );
